05589b96f8ec006270a7952990dab7bd911db67b
Hayden Chan <hchan@lcs.on.ca>
2023-11-14 12:49:22 -0500

    Initial Commit

---
+++ SomethingRotten/SomethingRottenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomethingRottenApp.swift
+//  SomethingRotten
+//
+//  Created by Hayden Chan on 2023-11-14.
+//
+
+import SwiftUI
+
+@main
+struct SomethingRottenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
